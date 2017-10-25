414e4bfb1f53440502dac27878d9bb276c3f058b
Dan <ds5ct@virginia.edu>
2017-10-25 13:03:29 -0400

    Hookup cleardb for heroku

--- config.php
+++ config.php
@@ -1,6 +1,17 @@
 <?php
-	$dbhost = 'localhost';
-	$dbuser = 'root';
-	$dbpass = '';
-	$dbname = 'shappy';
+	// $dbhost = 'localhost';
+	// $dbuser = 'root';
+	// $dbpass = '';
+	// $dbname = 'shappy';
+
+	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$cleardb_server = $cleardb_url['host'];
+	$cleardb_username = $cleardb_url['user'];
+	$cleardb_password = $cleardb_url['pass'];
+	$cleardb_db = substr($cleardb_url['path'], 1);
+
+	$dbhost = $cleardb_server;
+	$dbuser = $cleardb_username;
+	$dbpass = $cleardb_password;
+	$dbname = $cleardb_db;
 ?>
